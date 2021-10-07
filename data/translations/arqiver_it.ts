cea30f2e500b2c6870648093a8ac13c0d0805811
Weblate <noreply@weblate.org>
2021-10-07 16:08:12 +0200

    Weblate commits
    Co-authored-by: Baptiste H <baptiste.huchon@bechamail.fr>
    Co-authored-by: Standreas <standreas@riseup.net>
    Translate-URL: https://translate.lxqt-project.org/projects/tsujan/arqiver/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/tsujan/arqiver/it/
    Translation: Tsujan/Arqiver

--- data/translations/arqiver_it.ts
+++ data/translations/arqiver_it.ts
@@ -15,7 +15,7 @@
         <location filename="../../backends.cpp" line="631"/>
         <location filename="../../backends.cpp" line="664"/>
         <source>This file is a link but its target does not exist.</source>
-        <translation type="unfinished"></translation>
+        <translation>Questo file è un collegamento a un altro file inesistente.</translation>
     </message>
     <message>
         <location filename="../../backends.cpp" line="1047"/>
