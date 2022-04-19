ca07c2016b214ac4fbf6d49e07f455779df1a8c9
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2022-04-19 13:16:45 +0200

    Weblate commits (#57)
    Co-authored-by: kirusyaga <kirusyaga123@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/tsujan/arqiver/ru/
    Translation: Tsujan/Arqiver
    
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: kirusyaga <kirusyaga123@gmail.com>

--- data/translations/arqiver_ru.ts
+++ data/translations/arqiver_ru.ts
@@ -487,7 +487,7 @@ binary does not belong to libarchive.</source>
     <message>
         <location filename="../../mainWin.cpp" line="614"/>
         <source>READ-ONLY: VirtualBox Extension Pack (*.vbox-extpack)</source>
-        <translation type="unfinished"></translation>
+        <translation>Только для чтения: набор расширений VirtualBox (*.vbox-extpack)</translation>
     </message>
     <message>
         <location filename="../../mainWin.cpp" line="623"/>
