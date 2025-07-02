72edc8259307dc5eebeb3406b045bce5a47ba4b6
Tsu Jan <tsujan2000@gmail.com>
2025-07-03 01:18:48 +0330

    Translation update

--- data/translations/arqiver_eo.ts
+++ data/translations/arqiver_eo.ts
@@ -453,7 +453,7 @@ duumaĵo ne apartenus al libarchive.</translation>
         <location filename="../../mainWin.cpp" line="644"/>
         <location filename="../../mainWin.cpp" line="677"/>
         <source>LZ4 Compressed Archive</source>
-        <translation type="unfinished"></translation>
+        <translation>Kompaktigita Arkivo de LZ4</translation>
     </message>
     <message>
         <location filename="../../mainWin.cpp" line="645"/>
