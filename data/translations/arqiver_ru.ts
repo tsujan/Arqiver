08c23e23d8cb9f8de5f5b9936c694ebab88b2df1
Weblate <noreply@weblate.org>
2025-07-31 00:27:10 +0200

    Weblate commits
    Co-authored-by: Valera <ValeraDX@yandex.ru>
    Translate-URL: https://translate.lxqt-project.org/projects/tsujan/arqiver/ru/
    Translation: Tsujan/Arqiver

--- data/translations/arqiver_ru.ts
+++ data/translations/arqiver_ru.ts
@@ -453,7 +453,7 @@ binary does not belong to libarchive.</source>
         <location filename="../../mainWin.cpp" line="644"/>
         <location filename="../../mainWin.cpp" line="677"/>
         <source>LZ4 Compressed Archive</source>
-        <translation type="unfinished"></translation>
+        <translation>Архив с сжатием LZ4</translation>
     </message>
     <message>
         <location filename="../../mainWin.cpp" line="645"/>
