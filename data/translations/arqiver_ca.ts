c323cc6c978ac278e3f2f25a493f9771630c030f
Weblate <noreply@weblate.org>
2025-10-16 18:27:38 +0200

    Weblate commits
    Co-authored-by: Davidmp <opensusecatala@gmail.com>
    Co-authored-by: 이정희 <daemul72@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/tsujan/arqiver/ca/
    Translate-URL: https://translate.lxqt-project.org/projects/tsujan/arqiver/ko/
    Translation: Tsujan/Arqiver

--- data/translations/arqiver_ca.ts
+++ data/translations/arqiver_ca.ts
@@ -453,7 +453,7 @@ el binari no pertany a la libarchive.</translation>
         <location filename="../../mainWin.cpp" line="644"/>
         <location filename="../../mainWin.cpp" line="677"/>
         <source>LZ4 Compressed Archive</source>
-        <translation type="unfinished"></translation>
+        <translation>Arxiu comprimit LZ4</translation>
     </message>
     <message>
         <location filename="../../mainWin.cpp" line="645"/>
